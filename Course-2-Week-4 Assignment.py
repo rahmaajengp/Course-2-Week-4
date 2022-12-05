59c8ce564dca3fd11c9ed54b1d49bc5fe1a59f46
rahmaajengp <119779705+rahmaajengp@users.noreply.github.com>
2022-12-06 00:01:06 +0700

    Add files via upload

---
+++ Course-2-Week-4 Assignment.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **RAHMA AJENG PUSPITASARI**
+# ***
+# Data Scientist
+
+# *I am interested in Data Science because I would love to contribute towards affordable and quality healthcare in the future*
+
+# ### The below should print 'Hello World'
+
+# In[1]:
+
+
+print ("Hello World!")
+
+
+# ### Horizontal rule
+# ***
+# ### Bullet Lists
+# * This is a bullet list
+# * This is a bullet list
+# * This is a bullet list
+# ***
+# ### Numbered List
+# 1. And you can also create ordered lists
+# 2. by using numbers
+# 3. and listing new items in the lists 
+# 4. on their own lines
+# ***
+# ### Bold and Italice
+# * **This is a bullet item with bold words**
+# * *This is a bullet item with italicized words*
+# ***
+# ### Highlight
+# `Here is some code!`
+
+# In[ ]:
+
+
+
+
